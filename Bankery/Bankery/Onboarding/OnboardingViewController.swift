ad80ccd3d3695cc57cb56df5a27d591a2d6f45a5
luchocas234 <lfcastro2911@gmail.com>
2022-07-18 16:02:11 -0300

    l

---
+++ Bankery/Bankery/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,62 @@
+//
+//  OnboardingViewController.swift
+//  Bankery
+//
+//  Created by Luciano Federico Castro on 15/07/2022.
+//
+
+import Foundation
+import UIKit
+
+class OnboardingViewController : UIViewController{
+    
+    let stackView = UIStackView()
+    let imageView = UIImageView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension OnboardingViewController{
+    func style(){
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        //Image
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "Pixel-Apple")
+        
+        //label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.adjustsFontSizeToFitWidth = true
+        label.numberOfLines = 0
+        label.text = "Bankey is an app da da da, go to the store and download it"
+        
+    }
+    
+    func layout(){
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1 ),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+        
+    }
+}
+
+import Foundation
